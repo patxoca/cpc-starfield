66dc708b0875a32ace3526127115e7fe8cf50b67
Alexis Roda <alexis.roda.villalonga@gmail.com>
2020-08-12 12:10:48 +0200

    Moure els comentaris de funcions de "entity.c" a "entity.h".

--- src/managers/entity.c
+++ src/managers/entity.c
@@ -7,18 +7,12 @@ u8 num_entities;
 entity_t *free_ptr;
 
 
-//==============================================================================
-// Inicialitza el gestor d'entitats.
-
 void entity_mgr_init(void) {
     cpct_memset(entities, 0, sizeof(entities));
     num_entities = 0;
     free_ptr = entities;
 }
 
-
-//==============================================================================
-// Obté espai per una entitat.
 
 entity_t *entity_mgr_alloc(void) {
     entity_t *ptr = free_ptr;
@@ -31,8 +25,6 @@ entity_t *entity_mgr_alloc(void) {
     return ptr;
 }
 
-//==============================================================================
-// Marca una entitat per destruir-la.
 
 void entity_mgr_gc_mark(entity_t *e) __z88dk_fastcall {
     u8 tmp = e->type;
@@ -40,36 +32,6 @@ void entity_mgr_gc_mark(entity_t *e) __z88dk_fastcall {
     e->type = (tmp | ENTITY_DEAD_FLAG);
 }
 
-//==============================================================================
-// Allibera l'espai ocupat per les entitats marcades.
-
-/* void entity_mgr_gc_sweep(void) { */
-/*     entity_t *curr = entities; */
-/*     entity_t *end = free_ptr - 1; */
-/*     u8 count = num_entities; */
-/*     u8 foo = 0; */
-
-/*     while (count--) { */
-/*         if (!ENTITY_IS_ALIVE(curr)) { */
-/*             foo++; */
-/*             if (count) { */
-/*                 cpct_memcpy(curr, end, sizeof(entity_t)); */
-/*             } */
-/*             end--; */
-/*         } else { */
-/*             curr++; */
-/*         } */
-/*     } */
-/*     free_ptr = end + 1; */
-/*     num_entities -= foo; */
-/* } */
-
-//==============================================================================
-// Assigna el tipus.
-//
-// El bit superior del camp "type" s'utilitza internament per
-// registrar si l'entitat està viva o morta. Aquesta funció li assigna
-// un valor al tipus respectant aquest bit.
 
 void entity_mgr_set_type(entity_t *e, u8 type) {
     u8 tmp = e->type;
@@ -77,8 +39,6 @@ void entity_mgr_set_type(entity_t *e, u8 type) {
     e->type = (tmp & 0x80) | (type & 0x7f);
 }
 
-//==============================================================================
-// Itera sobre les entitats vives.
 
 void entity_mgr_foreach(void (*f)(entity_t *e) __z88dk_fastcall) __z88dk_fastcall {
     entity_t *p = entities;
@@ -91,9 +51,6 @@ void entity_mgr_foreach(void (*f)(entity_t *e) __z88dk_fastcall) __z88dk_fastcal
     }
 }
 
-
-//==============================================================================
-// Retorna el nombre d'entitats lliures
 
 u8 entity_mgr_empty_slots_count(void) {
     return MAX_ENTITIES - num_entities;
--- src/managers/entity.h
+++ src/managers/entity.h
@@ -15,10 +15,50 @@ typedef struct {
 } entity_t;
 
 
+//==============================================================================
+//   Inicialitza el gestor d'entitats.
+//
+
 void entity_mgr_init(void);
+
+
+//==============================================================================
+//   Obté espai per una entitat.
+//
+
 entity_t *entity_mgr_alloc(void);
+
+
+//==============================================================================
+//   Marca una entitat per destruir-la.
+//
 void entity_mgr_gc_mark(entity_t *e) __z88dk_fastcall;
+
+
+//==============================================================================
+//   Allibera l'espai ocupat per les entitats marcades.
+//
+
 void entity_mgr_gc_sweep(void);
+
+
+//==============================================================================
+//   Assigna el tipus.
+//
+//   El bit superior del camp "type" s'utilitza internament per
+//   registrar si l'entitat està viva o morta. Aquesta funció li
+//   assigna un valor al tipus respectant aquest bit.
+
 void entity_mgr_set_type(entity_t *e, u8 type);
+
+
+//==============================================================================
+//   Itera sobre les entitats.
+
 void entity_mgr_foreach(void (*f)(entity_t *e) __z88dk_fastcall) __z88dk_fastcall;
+
+
+//==============================================================================
+//   Retorna el nombre d'entitats lliures
+
 u8 entity_mgr_empty_slots_count(void);
